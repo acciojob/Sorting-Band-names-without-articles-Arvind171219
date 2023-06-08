5600e469b8198e9a664c72b38a84ce2bf3c705be
Arvind kumar <74968170+Arvind171219@users.noreply.github.com>
2023-06-08 17:35:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
+function sortBandNames(bandNames) {
+  const articles = ['a', 'an', 'the'];
 
+  const sortedNames = bandNames.sort((a, b) => {
+    const nameA = a.toLowerCase().replace(/^(a|an|the)\s+/i, '');
+    const nameB = b.toLowerCase().replace(/^(a|an|the)\s+/i, '');
+    return nameA.localeCompare(nameB);
+  });
+
+  return sortedNames;
+}
+
+// Example usage
+const bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+const sortedBandNames = sortBandNames(bandNames);
+
+const ulElement = document.getElementById('band');
+sortedBandNames.forEach((name) => {
+  const liElement = document.createElement('li');
+  liElement.textContent = name;
+  ulElement.appendChild(liElement);
+});
